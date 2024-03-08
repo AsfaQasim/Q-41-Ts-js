11efd3c0b17677b0b189bdc152d368d141c02843
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-08 09:18:28 -0800

    Q 41 of Ts/js & node js of 45 assignment.
    Q 41 of Ts/js & node js of 45 assignment.

---
+++ main.js
@@ -0,0 +1,8 @@
+// Define an array of magician's names
+var magicians = ["Ahmed", "Usman", "Fakar", "Zakir"];
+// Function to show magicians
+function show_magicians(magicians) {
+    magicians.forEach(function (magician) { return console.log(magician); });
+}
+// Call the function to display the magicians
+show_magicians(magicians);
---
+++ main.ts
@@ -0,0 +1,14 @@
+
+
+// Define an array of magician's names
+const magicians: string[] = ["Ahmed", "Usman", "Fakar", "Zakir"];
+
+// Function to show magicians
+function show_magicians(magicians: string[]): void {
+    magicians.forEach(magician => console.log(magician));
+}
+
+// Call the function to display the magicians
+show_magicians(magicians);
+
+
